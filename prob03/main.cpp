aff0901786fdead80ee17c766f314ff94eb326b0
shrewdrew21 <shrewdrew21@cs104u-4e.acad.fullerton.edu>
2019-09-28 15:34:02 -0700

    random rectangle

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,17 @@
 // Random Rectangle
+
+#include <iostream>
+int main()
+{
+std::cout << "#"<<std::endl;
+std::cout << "#"<<std::endl;
+std::cout << "#"<<std::endl;
+
+int min = 3;
+unsigned seed = time(0);
+srand(seed);
+int max = 40;
+double num = rand() % max + 40;
+
+  return 0;
+}
