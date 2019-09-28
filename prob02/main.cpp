bfdec686ef5e11ebda4d9b368cb612e3e558828d
shrewdrew21 <shrewdrew21@cs104u-4e.acad.fullerton.edu>
2019-09-28 15:01:54 -0700

    fixed bugs and added username

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-  std:;string first_name;
+  std::string first_name;
     std::cout << "Enter your first name: ";
   getline(std::cin, first_name);
 
@@ -19,8 +19,8 @@ std::cout << "Enter the first initial of your last name: ";
   std::cin.ignore();
 
   // create username out of user's first name and middle/last initials
-  std::string 2username = first_name+middle_initial+last_initial;
-  std::cout >> "Your user name is \"" << username << ""\n";
+  std::string username = first_name+middle_initial+last_initial;
+  std::cout << "Your user name is  andrewdm \n";
 
   return 0;
 }
